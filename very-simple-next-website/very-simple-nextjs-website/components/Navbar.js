1d2e9b7e2759f8faa25aa97bb2f8a40a8fe353a6
TechLounge777 <msblanche777@gmail.com>
2021-08-06 21:40:17 +0200

    added links to the navbar component

--- very-simple-next-website/very-simple-nextjs-website/components/Navbar.js
+++ very-simple-next-website/very-simple-nextjs-website/components/Navbar.js
@@ -1,14 +1,38 @@
 import styled from "styled-components"
+import Link from 'next/link'
 
 const Nav = styled.nav`
     height: 80px;
     background: #000;
+    color: #fff; 
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     `;
+
+const StyledLink = styled.a`
+    padding: 0rem 2rem;
+`;
 
 const Navbar = () => {
     return (
         <Nav>
-            <h1>Navbar</h1>
+            <div>
+                <Link href="/">
+                    <StyledLink>NXT</StyledLink>
+                </Link>
+            </div>
+            <div>
+                <Link href="/">
+                    <StyledLink>Home</StyledLink>
+                </Link>
+                <Link href="/about">
+                    <StyledLink>About</StyledLink>
+                </Link>
+                <Link href="/contact">
+                    <StyledLink>Contact</StyledLink>
+                </Link>
+            </div>
         </Nav>
     )
 }
