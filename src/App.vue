be83aed3a9a65a39d93cead6c6b48a06260b0d4b
TechLounge777 <msblanche777@gmail.com>
2021-11-23 12:44:45 +0100

    added a todo item

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>{{ mainTitle }}</h1>
+    <div v-for="item in items" v-bind:key="item.id">
+      <p>{{ item.title }}</p>
+    </div>
+    <button id="btn" @click="greet">Click me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mainTitle: "Hello Vue!",
+      items: [
+        { title: "Shopping", completed: false, id: 1 },
+        { title: "Learn JavaScript", completed: false, id: 2 },
+      ],
+    };
+  },
+  methods: {
+    greet() {
+      alert("Hello");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+  color: red;
+}
+#btn {
+  margin-top: 35px;
+}
+li {
+  list-style: none;
+  color: green;
+}
+</style>
